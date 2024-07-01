dceadd2f4b9be3e9f1f5472c60214d0dae2391f9
6ogunt48 <114788338+6ogunt48@users.noreply.github.com>
2024-07-01 15:35:50 +0100

    removed redundant file opening

--- fileops/file_operations.go
+++ fileops/file_operations.go
@@ -11,86 +11,72 @@ type FileOperations struct {
 	ShowLines bool
 	ShowWords bool
 	ShowBytes bool
+	ShowChars bool
 }
 
-func (ops *FileOperations) GetFileByte() (int64, error) {
-	file, err := os.Open(ops.Filename)
-	if err != nil {
-		return 0, err
-	}
-	defer file.Close()
-
+func (ops *FileOperations) GetFileByte(file *os.File) (int64, error) {
 	fileInfo, err := file.Stat()
 	if err != nil {
 		return 0, err
 	}
-
 	return fileInfo.Size(), nil
 }
 
-func (ops *FileOperations) CountLines() (int, error) {
-	file, err := os.Open(ops.Filename)
-	if err != nil {
-		return 0, err
-	}
-	defer file.Close()
-
+func (ops *FileOperations) CountLines(file *os.File) (int, error) {
 	scanner := bufio.NewScanner(file)
 	lineCount := 0
 	for scanner.Scan() {
 		lineCount++
 	}
-
 	return lineCount, scanner.Err()
 }
 
-func (ops *FileOperations) CountWords() (int, error) {
-	file, err := os.Open(ops.Filename)
-	if err != nil {
-		return 0, err
-	}
-	defer file.Close()
-
+func (ops *FileOperations) CountWords(file *os.File) (int, error) {
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanWords)
 	wordCount := 0
 	for scanner.Scan() {
 		wordCount++
 	}
-
 	return wordCount, scanner.Err()
 }
 
-func (ops *FileOperations) CountAll() (int, int, int64, error) {
-	file, err := os.Open(ops.Filename)
-	if err != nil {
-		return 0, 0, 0, err
+func (ops *FileOperations) CountChars(file *os.File) (int, error) {
+	scanner := bufio.NewScanner(file)
+	charCount := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		charCount += len(line)
 	}
-	defer file.Close()
+	return charCount, scanner.Err()
+}
+
+func (ops *FileOperations) CountAll(file *os.File) (int, int, int64, int, error) {
 	lineCount := 0
 	wordCount := 0
 	byteCount := int64(0)
+	charCount := 0
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		lineCount++
 		line := scanner.Text()
-		byteCount += int64(len(line)) + 1
+		byteCount += int64(len(line)) + 1 // Adding 1 for the newline character
+		charCount += len(line)
 
 		lineScanner := bufio.NewScanner(strings.NewReader(line))
 		lineScanner.Split(bufio.ScanWords)
 		for lineScanner.Scan() {
 			wordCount++
 		}
-
 		if err := lineScanner.Err(); err != nil {
-			return 0, 0, 0, err
+			return 0, 0, 0, 0, err
 		}
 	}
 
 	if err := scanner.Err(); err != nil {
-		return 0, 0, 0, err
+		return 0, 0, 0, 0, err
 	}
 
-	return lineCount, wordCount, byteCount, nil
+	return lineCount, wordCount, byteCount, charCount, nil
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"wcgo/fileops"
 )
 
@@ -11,27 +12,35 @@ func main() {
 	flag.BoolVar(&ops.ShowLines, "l", false, "show line count")
 	flag.BoolVar(&ops.ShowWords, "w", false, "show word count")
 	flag.BoolVar(&ops.ShowBytes, "c", false, "show byte count")
+	flag.BoolVar(&ops.ShowChars, "m", false, "show character count")
 	flag.Parse()
 
 	if len(flag.Args()) < 1 {
-		fmt.Println("Usage: wcgo [-l] [-w] [-c] <filename>")
+		fmt.Println("Usage: wcgo [-l] [-w] [-c] [-m] <filename>")
 		return
 	}
 
 	ops.Filename = flag.Args()[0]
 
-	if !ops.ShowLines && !ops.ShowWords && !ops.ShowBytes {
-		lines, words, bytes, err := ops.CountAll()
+	file, err := os.Open(ops.Filename)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	if !ops.ShowLines && !ops.ShowWords && !ops.ShowBytes && !ops.ShowChars {
+		lines, words, bytes, chars, err := ops.CountAll(file)
 		if err != nil {
 			fmt.Println("Error:", err)
 		} else {
-			fmt.Printf("%8d %8d %d %s\n", lines, words, bytes, ops.Filename)
+			fmt.Printf("%8d %8d %8d %8d %s\n", lines, words, bytes, chars, ops.Filename)
 		}
 		return
 	}
 
 	if ops.ShowLines {
-		lines, err := ops.CountLines()
+		lines, err := ops.CountLines(file)
 		if err != nil {
 			fmt.Println("Error:", err)
 		} else {
@@ -40,7 +49,7 @@ func main() {
 	}
 
 	if ops.ShowWords {
-		words, err := ops.CountWords()
+		words, err := ops.CountWords(file)
 		if err != nil {
 			fmt.Println("Error:", err)
 		} else {
@@ -49,11 +58,20 @@ func main() {
 	}
 
 	if ops.ShowBytes {
-		bytes, err := ops.GetFileByte()
+		bytes, err := ops.GetFileByte(file)
 		if err != nil {
 			fmt.Println("Error:", err)
 		} else {
 			fmt.Printf("%8d %s\n", bytes, ops.Filename)
+		}
+	}
+
+	if ops.ShowChars {
+		chars, err := ops.CountChars(file)
+		if err != nil {
+			fmt.Println("Error:", err)
+		} else {
+			fmt.Printf("%8d %s\n", chars, ops.Filename)
 		}
 	}
 }
